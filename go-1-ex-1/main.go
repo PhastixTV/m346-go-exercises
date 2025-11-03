c808436776462dc41df0e2927842ad63e56eb334
PhastixTV <philipp.sidler0206@gmail.com>
2025-11-03 14:44:08 +0100

    Aufgabe 1 gelöst

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName = "Philipp"
+	var lastName = "Sidler"
+	var dayOfBirth = 2
+	var monthOfBirth = 06
+	var yearOfBirth = 2008
+	var numberOfSiblings = 4
+	var heightInMeters = 1.72
+	var zodiacSign = '♊'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
